ba5441c4258e8d9aeed7f2eb013469628372c65f
liujch16 <liujch16@outlook.com>
2021-07-19 11:02:33 +0800

    修改

--- file_practice/file_practice1.py
+++ file_practice/file_practice1.py
@@ -6,5 +6,10 @@ with open(file_path) as file_object:#关键字with使python在合适的时候关
 pi_string = ''#在此处定义了一个空字符串
 for line in lines:
     pi_string += line.strip()#strip()方法删除每个line元素左边的换行符
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the Pi")
+else:
+    print("Your birthday does not appear in the Pi")
 print(pi_string)
 print(len(pi_string))
