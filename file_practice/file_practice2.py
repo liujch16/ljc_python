ffd6c1c97ecbccbd19a8de946dc5078eea743138
liujch16 <liujch16@outlook.com>
2021-07-19 11:34:46 +0800

    又是一个修改

--- file_practice/file_practice2.py
+++ file_practice/file_practice2.py
@@ -1,5 +1,11 @@
 file_path = 'D:\\pythonworkspace1\\vscode_python\\file_practice\\learning_python.txt'
 
 with open(file_path) as file_project:
-    contents = file_project.read()
-    print(contents)
+    lines = file_project.readlines()
+
+str_1 = ''
+for line in lines:
+    line = line.replace('Python', 'C')
+    str_1 += line
+
+print(str_1)
